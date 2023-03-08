501f5777de93842f44c41b67b76d73e0397749c2
Ling Fung Chan <justinlfchan@gmail.com>
2023-03-08 12:49:49 +0000

    add review to restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [ :index, :show ] do
+    resources :reviews, only: :create
+  end
 end
